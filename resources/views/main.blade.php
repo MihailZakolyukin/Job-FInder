bf0253208ee370a6a9e93e80ef6ec4dc86355468
Mihail.Zakolyukin <mihail.zakolyukin2015@gmail.com>
2021-02-22 19:55:47 +0500

    Added main view

---
+++ resources/views/main.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Работа в Челябинске, Job-Finder</title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
+	<link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+<div class="head"></div>
+<div class="body"></div>
+<div class="footer"></div>
+</body>
+</html>
